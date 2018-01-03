cce5051a0235be2cb52b036666f310d3e760b71a
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-01-03 16:40:24 +0300

    Main functions added; README added

--- bars.py
+++ bars.py
@@ -1,21 +1,51 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as input_file:
+        bars_list = json.load(input_file)
+    return bars_list
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(bars_list):
+    biggest_bar = (max(bars_list, key=lambda bar: bar['SeatsCount']))
+    print('Самый большой бар - ' + biggest_bar['Name'])
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(bars_list):
+    smallest_bar = (min(bars_list, key=lambda bar: bar['SeatsCount']))
+    print('Самый маленький бар - ' + smallest_bar['Name'])
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(bars_list, longitude, latitude):
+    closest_bar = (min(bars_list, key=lambda bar:
+                   abs(float(bar['Latitude_WGS84']) - latitude) +
+                   abs(float(bar['Longitude_WGS84']) - longitude)))
+
+    print('Ближайший бар - ' + closest_bar['Name'] +
+          ' (Широта ' + closest_bar['Latitude_WGS84'] +
+          ' Долгота ' + closest_bar['Longitude_WGS84'] + ')')
+
+
+def get_user_coordinates(coordinate):
+    try:
+        print('Введите значение ' + coordinate, end=' ')
+        return float(input())
+    except ValueError:
+        print('Широта и долгота должны быть числами!')
+        sys.exit()
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        bars_list = load_data(sys.argv[1])
+        latitude = get_user_coordinates('latitude')
+        longitude = get_user_coordinates('longitude')
+        get_closest_bar(bars_list, longitude, latitude)
+        get_biggest_bar(bars_list)
+        get_smallest_bar(bars_list)
+    except (IndexError, FileNotFoundError):
+        print("Input file is not specified or missed")
+    except json.decoder.JSONDecodeError:
+        print("File is incorrect, broken or empty")
