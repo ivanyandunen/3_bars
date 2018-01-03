ba870ad6b5bd8f1048d14315e3ef6ad8d69cffcf
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-01-03 23:21:58 +0300

    Убран бесконечный цикл while; вывод вынесен в main; добавлена ссылка на скачивание json в README; сложение строк заменено форматированием

--- bars.py
+++ bars.py
@@ -4,48 +4,51 @@ import sys
 
 def load_data(filepath):
     with open(filepath) as input_file:
-        bars_list = json.load(input_file)
-    return bars_list
+        return json.load(input_file)
 
 
 def get_biggest_bar(bars_list):
-    biggest_bar = (max(bars_list, key=lambda bar: bar['SeatsCount']))
-    print('Самый большой бар - ' + biggest_bar['Name'])
+    return max(bars_list, key=lambda bar: bar['SeatsCount'])
 
 
 def get_smallest_bar(bars_list):
-    smallest_bar = (min(bars_list, key=lambda bar: bar['SeatsCount']))
-    print('Самый маленький бар - ' + smallest_bar['Name'])
+    return min(bars_list, key=lambda bar: bar['SeatsCount'])
 
 
 def get_closest_bar(bars_list, longitude, latitude):
-    closest_bar = (min(bars_list, key=lambda bar:
-                   abs(float(bar['Latitude_WGS84']) - latitude) +
-                   abs(float(bar['Longitude_WGS84']) - longitude)))
-
-    print('Ближайший бар - ' + closest_bar['Name'] +
-          ' (Широта ' + closest_bar['Latitude_WGS84'] +
-          ' Долгота ' + closest_bar['Longitude_WGS84'] + ')')
-
-
-def get_user_coordinates(coordinate):
-    print('Введите значение ' + coordinate, end=' ')
-    coordinate = input()
-    while not coordinate.isnumeric():
-        print('Широта и долгота должны быть числами!')
-        coordinate = input()
-    return float(coordinate)
+    return (min(bars_list, key=lambda bar:
+                abs(float(bar['Latitude_WGS84']) - latitude) +
+                abs(float(bar['Longitude_WGS84']) - longitude)))
 
 
 if __name__ == '__main__':
     try:
         bars_list = load_data(sys.argv[1])
-        latitude = get_user_coordinates('latitude')
-        longitude = get_user_coordinates('longitude')
-        get_closest_bar(bars_list, longitude, latitude)
-        get_biggest_bar(bars_list)
-        get_smallest_bar(bars_list)
+
+        print('Введите широту: ', end=' ')
+        latitude = float(input())
+        print('Введите долготу: ', end=' ')
+        longitude = float(input())
+
+        closest_bar = get_closest_bar(bars_list, longitude, latitude)
+        print('Ближайший бар - {0}, Широта {1}, Долгота {2}'.format(
+            closest_bar['Name'],
+            closest_bar['Latitude_WGS84'],
+            closest_bar['Longitude_WGS84']))
+
+        biggest_bar = get_biggest_bar(bars_list)
+        print('Самый большой бар - {}, количество мест {}'.format(
+            biggest_bar['Name'],
+            biggest_bar['SeatsCount']))
+
+        smallest_bar = get_smallest_bar(bars_list)
+        print('Самый маленький бар - {}, количество мест {}'.format(
+            smallest_bar['Name'],
+            smallest_bar['SeatsCount']))
+
     except (IndexError, FileNotFoundError):
-        print("Input file is not specified or missed")
+        print('Input file is not specified or missed')
     except json.decoder.JSONDecodeError:
-        print("File is incorrect, broken or empty")
+        print('File is incorrect, broken or empty')
+    except ValueError:
+        print('Широта и долгота должны быть цифрами')
