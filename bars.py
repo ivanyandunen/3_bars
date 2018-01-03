72311737f5bfdd7ca76ebe2b5366ca90ab50adad
Ivan Yandunen <34913063+ivanyandunen@users.noreply.github.com>
2018-01-04 00:39:14 +0300

    Исправил форматирование в соответствие с гайдом

--- bars.py
+++ bars.py
@@ -17,8 +17,8 @@ def get_smallest_bar(bars_list):
 
 def get_closest_bar(bars_list, longitude, latitude):
     return min(bars_list, key=lambda bar:
-                abs(float(bar['Latitude_WGS84']) - latitude) +
-                abs(float(bar['Longitude_WGS84']) - longitude))
+               abs(float(bar['Latitude_WGS84']) - latitude) +
+               abs(float(bar['Longitude_WGS84']) - longitude))
 
 
 if __name__ == '__main__':
@@ -31,20 +31,32 @@ if __name__ == '__main__':
         longitude = float(input())
 
         closest_bar = get_closest_bar(bars_list, longitude, latitude)
-        print('Ближайший бар - {0}, Широта {1}, Долгота {2}'.format(
-            closest_bar['Name'],
-            closest_bar['Latitude_WGS84'],
-            closest_bar['Longitude_WGS84']))
+        print(
+            'Ближайший бар - {0}, Широта {1}, Долгота {2}'
+            .format(
+                closest_bar['Name'],
+                closest_bar['Latitude_WGS84'],
+                closest_bar['Longitude_WGS84']
+            )
+        )
 
         biggest_bar = get_biggest_bar(bars_list)
-        print('Самый большой бар - {}, количество мест {}'.format(
-            biggest_bar['Name'],
-            biggest_bar['SeatsCount']))
+        print(
+            'Самый большой бар - {}, количество мест {}'
+            .format(
+                biggest_bar['Name'],
+                biggest_bar['SeatsCount']
+            )
+        )
 
         smallest_bar = get_smallest_bar(bars_list)
-        print('Самый маленький бар - {}, количество мест {}'.format(
-            smallest_bar['Name'],
-            smallest_bar['SeatsCount']))
+        print(
+            'Самый маленький бар - {}, количество мест {}'
+            .format(
+                smallest_bar['Name'],
+                smallest_bar['SeatsCount']
+            )
+        )
 
     except (IndexError, FileNotFoundError):
         print('Input file is not specified or missed')
